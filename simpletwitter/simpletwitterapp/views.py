0d80fab62dcf556a480863c4fcf8927947455fba
Bishakh Ghosh <ghoshbishakh@gmail.com>
2021-08-21 10:28:43 +0530

    query and display tweet

--- simpletwitter/simpletwitterapp/views.py
+++ simpletwitter/simpletwitterapp/views.py
@@ -2,6 +2,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Tweet
+
 # Create your views here.
 def index(request):
-    return render(request, "simpletwitterapp/index.html", {})
+    all_tweets = Tweet.objects.all()
+    print(all_tweets)
+    context = {}
+    context["all_tweets"] = all_tweets
+    return render(request, "simpletwitterapp/index.html", context)
