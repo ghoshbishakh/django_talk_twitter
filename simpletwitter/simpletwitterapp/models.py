d3bd9826795642219946ddb329d9d457e6dfd40d
Bishakh Ghosh <ghoshbishakh@gmail.com>
2021-08-21 10:23:43 +0530

    add model

--- simpletwitter/simpletwitterapp/models.py
+++ simpletwitter/simpletwitterapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Tweet(models.Model):
+    username = models.CharField(max_length=200)
+    content = models.CharField(max_length=400)
+    datetime = models.DateTimeField(auto_now_add=True)
